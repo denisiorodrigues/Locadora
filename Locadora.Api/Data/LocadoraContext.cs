a331db8de634af60e6327b844e4c4fc4f0eb3c45
Denisio Rodrigues <denisio@ymail.com>
2026-02-02 09:09:46 -0300

    Configucação da base de addos MySQL

---
+++ Locadora.Api/Data/LocadoraContext.cs
@@ -0,0 +1,14 @@
+﻿using Locadora.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locadora.Api.Data;
+
+public class LocadoraContext : DbContext
+{
+    public LocadoraContext(DbContextOptions<LocadoraContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+}
--- Locadora.Api/Program.cs
+++ Locadora.Api/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Locadora.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("LocadoraConnection");
+builder.Services.AddDbContext<LocadoraContext>(options => 
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
