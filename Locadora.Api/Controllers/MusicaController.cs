3235827d03fad0209e539b1968480531275af500
Denisio Rodrigues <denisio@ymail.com>
2026-02-06 09:36:45 -0300

    Listar musicas api externa

---
+++ Locadora.Api/Controllers/MusicaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Locadora.Api.Controllers;
+
+[ApiController]
+[Route("api/musicas")]
+public class MusicaController : ControllerBase
+{
+    private const string URL_MUSICAS = "https://guilhermeonrails.github.io/api-csharp-songs/songs.json";
+    
+    /// <summary>
+    /// Obter a lista de musicas de uma API externa
+    /// </summary>
+    /// <returns code="200">Lista de musicas</returns>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> ListarMusicas()
+    {
+        using (HttpClient client = new  HttpClient())
+        {
+            var musicas = await client.GetStringAsync(URL_MUSICAS);
+            return Ok(musicas);
+        }
+    }
+}
